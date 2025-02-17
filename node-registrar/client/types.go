4dd5b29ce990d2530f79bd1ee8812f01965b5cd5
Eslam-Nawara <eslamnawara44@gmail.com>
2025-02-17 15:41:17 +0200

    add signature for farm functions

---
+++ node-registrar/client/types.go
@@ -0,0 +1,66 @@
+package client
+
+import (
+	"time"
+)
+
+type Account struct {
+	TwinID    uint64   `json:"twin_id"`
+	Relays    []string `json:"relays"`      // Optional list of relay domains
+	RMBEncKey string   `json:"rmb_enc_key"` // Optional base64 encoded public key for rmb communication
+	PublicKey string   `json:"public_key"`
+}
+
+type Farm struct {
+	FarmID    uint64 `json:"farm_id"`
+	FarmName  string `json:"farm_name"`
+	TwinID    uint64 `json:"twin_id"`
+	Dedicated bool   `json:"dedicated"`
+}
+
+type Node struct {
+	NodeID        uint64         `json:"node_id"`
+	FarmID        uint64         `json:"farm_id"`
+	TwinID        uint64         `json:"twin_id"`
+	Location      Location       `json:"location"`
+	Resources     Resources      `json:"resources"`
+	Interfaces    []Interface    `json:"interface"`
+	SecureBoot    bool           `json:"secure_boot"`
+	Virtualized   bool           `json:"virtualized"`
+	SerialNumber  string         `json:"serial_number"`
+	UptimeReports []UptimeReport `json:"uptime"`
+	Approved      bool
+}
+
+type UptimeReport struct {
+	ID         uint64
+	NodeID     uint64        `json:"node_id"`
+	Duration   time.Duration `json:"duration"`
+	Timestamp  time.Time     `json:"timestamp"`
+	WasRestart bool          `json:"was_restart"`
+}
+
+type ZosVersion struct {
+	Version       string `json:"version"`
+	SafeToUpgrade bool   `json:"safe_to_upgrade"`
+}
+
+type Interface struct {
+	Name string `json:"name"`
+	Mac  string `json:"mac"`
+	IPs  string `json:"ips"`
+}
+
+type Resources struct {
+	HRU uint64 `json:"hru"`
+	SRU uint64 `json:"sru"`
+	CRU uint64 `json:"cru"`
+	MRU uint64 `json:"mru"`
+}
+
+type Location struct {
+	Country   string `json:"country"`
+	City      string `json:"city"`
+	Longitude string `json:"longitude"`
+	Latitude  string `json:"latitude"`
+}

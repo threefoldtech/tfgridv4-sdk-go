8188c0936cf2c3905a23b6aa587b3141ecbb2ebb
rawdaGastan <rody.rawda.rr@gmail.com>
2025-02-27 14:27:49 +0200

    add rmb client in go

---
+++ rmb-sdk-go/go.mod
@@ -0,0 +1,52 @@
+module github.com/threefoldtech/tfgrid-sdk-go/rmb-sdk-go
+
+go 1.21
+
+require (
+	github.com/ChainSafe/go-schnorrkel v1.1.0
+	github.com/decred/dcrd/dcrec/secp256k1/v4 v4.3.0
+	github.com/golang-jwt/jwt v3.2.2+incompatible
+	github.com/golang/mock v1.6.0
+	github.com/gomodule/redigo v2.0.0+incompatible
+	github.com/google/uuid v1.6.0
+	github.com/gorilla/websocket v1.5.3
+	github.com/gtank/merlin v0.1.1
+	github.com/hashicorp/go-multierror v1.1.1
+	github.com/pkg/errors v0.9.1
+	github.com/rs/zerolog v1.33.0
+	github.com/stretchr/testify v1.10.0
+	github.com/threefoldtech/tfchain/clients/tfchain-client-go v0.0.0-20241007205731-5e76664a3cc4
+	gonum.org/v1/gonum v0.15.0
+	google.golang.org/protobuf v1.34.1
+)
+
+require (
+	github.com/cenkalti/backoff v2.2.1+incompatible // indirect
+	github.com/centrifuge/go-substrate-rpc-client/v4 v4.0.12 // indirect
+	github.com/cosmos/go-bip39 v1.0.0 // indirect
+	github.com/davecgh/go-spew v1.1.2-0.20180830191138-d8f796af33cc // indirect
+	github.com/deckarep/golang-set v1.8.0 // indirect
+	github.com/decred/base58 v1.0.5 // indirect
+	github.com/decred/dcrd/crypto/blake256 v1.0.1 // indirect
+	github.com/ethereum/go-ethereum v1.11.6 // indirect
+	github.com/go-stack/stack v1.8.1 // indirect
+	github.com/gtank/ristretto255 v0.1.2 // indirect
+	github.com/hashicorp/errwrap v1.1.0 // indirect
+	github.com/holiman/uint256 v1.2.3 // indirect
+	github.com/jbenet/go-base58 v0.0.0-20150317085156-6237cf65f3a6 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
+	github.com/mimoo/StrobeGo v0.0.0-20220103164710-9a04d6ca976b // indirect
+	github.com/pierrec/xxHash v0.1.5 // indirect
+	github.com/pmezard/go-difflib v1.0.1-0.20181226105442-5d4384ee4fb2 // indirect
+	github.com/rogpeppe/go-internal v1.11.0 // indirect
+	github.com/rs/cors v1.10.1 // indirect
+	github.com/shirou/gopsutil v3.21.11+incompatible // indirect
+	github.com/tklauser/go-sysconf v0.3.11 // indirect
+	github.com/vedhavyas/go-subkey v1.0.3 // indirect
+	github.com/yusufpapurcu/wmi v1.2.2 // indirect
+	golang.org/x/crypto v0.28.0 // indirect
+	golang.org/x/sys v0.26.0 // indirect
+	gopkg.in/natefinch/npipe.v2 v2.0.0-20160621034901-c1b8fa8bdcce // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+)
---
+++ rmb-sdk-go/peer/connection.go
@@ -0,0 +1,221 @@
+package peer
+
+import (
+	"context"
+	"fmt"
+	"io"
+	"net/http"
+	"time"
+
+	"github.com/gorilla/websocket"
+	"github.com/pkg/errors"
+	"github.com/rs/zerolog/log"
+	substrate "github.com/threefoldtech/tfchain/clients/tfchain-client-go"
+)
+
+const (
+	pongWait     = 40 * time.Second
+	pingInterval = 20 * time.Second
+)
+
+var errTimeout = fmt.Errorf("connection timeout")
+
+// InnerConnection holds the required state to create a self healing websocket connection to the rmb relay.
+type InnerConnection struct {
+	twinID   uint32
+	session  string
+	identity substrate.Identity
+	url      string
+	writer   chan send
+}
+
+type send struct {
+	data []byte
+	err  chan error
+}
+
+func (s *send) reply(ctx context.Context, err error) error {
+	defer func() {
+		if r := recover(); r != nil {
+			log.Debug().Msgf("recovered from panic: %v", r)
+		}
+	}()
+
+	select {
+	case <-ctx.Done():
+		return ctx.Err()
+	case s.err <- err:
+		return err
+	}
+}
+
+// Reader is a channel that receives incoming messages
+type Reader <-chan []byte
+
+func (r Reader) Read() []byte {
+	return <-r
+}
+
+// NewConnection creates a new InnerConnection instance
+func NewConnection(identity substrate.Identity, url string, session string, twinID uint32) InnerConnection {
+	return InnerConnection{
+		twinID:   twinID,
+		identity: identity,
+		url:      url,
+		session:  session,
+		writer:   make(chan send),
+	}
+}
+
+func (c *InnerConnection) reader(ctx context.Context, cancel context.CancelFunc, con *websocket.Conn, reader chan []byte) {
+	for {
+		typ, data, err := con.ReadMessage()
+		if err != nil {
+			log.Debug().Err(err).Msg("failed to read message")
+			cancel()
+			return
+		}
+
+		if typ != websocket.BinaryMessage {
+			log.Error().Msg("invalid message type received")
+			cancel()
+			return
+		}
+
+		select {
+		case <-ctx.Done():
+			return
+		case reader <- data:
+		}
+	}
+}
+
+func (c *InnerConnection) send(ctx context.Context, data []byte) error {
+	resp := make(chan error)
+	defer close(resp)
+
+	s := send{
+		data: data,
+		err:  resp,
+	}
+
+	select {
+	case c.writer <- s:
+	case <-ctx.Done():
+		return ctx.Err()
+	case <-time.After(2 * time.Second):
+		return errTimeout
+	}
+
+	select {
+	case err := <-resp:
+		return err
+	case <-ctx.Done():
+		return ctx.Err()
+	}
+}
+
+func (c *InnerConnection) loop(ctx context.Context, con *websocket.Conn, output chan []byte) error {
+	defer con.Close()
+
+	local, cancel := context.WithCancel(ctx)
+	defer cancel()
+
+	pong := make(chan byte)
+	con.SetPongHandler(func(appData string) error {
+		select {
+		case pong <- 1:
+		default:
+		}
+		return nil
+	})
+
+	outputCh := make(chan []byte)
+	defer close(outputCh)
+
+	go c.reader(local, cancel, con, outputCh)
+
+	lastPong := time.Now()
+	for {
+		select {
+		case <-ctx.Done():
+			return ctx.Err()
+		case <-local.Done():
+			return nil // error happened with the connection, return nil to try again
+		case data := <-outputCh:
+			output <- data
+			lastPong = time.Now()
+		case sent := <-c.writer:
+			err := con.WriteMessage(websocket.BinaryMessage, sent.data)
+			if replyErr := sent.reply(ctx, err); replyErr != nil {
+				return err
+			}
+		case <-pong:
+			lastPong = time.Now()
+		case <-time.After(pingInterval):
+			if err := con.WriteControl(websocket.PingMessage, nil, time.Now().Add(10*time.Second)); err != nil {
+				return err
+			}
+
+			if time.Since(lastPong) > pongWait {
+				return fmt.Errorf("connection stalling")
+			}
+		}
+	}
+}
+
+// Start initiates the websocket connection
+func (c *InnerConnection) Start(ctx context.Context, output chan []byte) {
+	go func() {
+		defer close(output)
+		defer close(c.writer)
+		for {
+			err := c.listenAndServe(ctx, output)
+			if err == context.Canceled {
+				break
+			} else if err != nil {
+				log.Error().Err(err).Send()
+			}
+
+			<-time.After(2 * time.Second)
+		}
+	}()
+}
+
+// listenAndServe creates the websocket connection, and if successful, listens for and serves incoming and outgoing messages.
+func (c *InnerConnection) listenAndServe(ctx context.Context, output chan []byte) error {
+	con, err := c.connect()
+	if err != nil {
+		return errors.Wrap(err, "failed to reconnect")
+	}
+
+	return c.loop(ctx, con, output)
+}
+
+func (c *InnerConnection) connect() (*websocket.Conn, error) {
+	token, err := NewJWT(c.identity, c.twinID, c.session, 60)
+	if err != nil {
+		return nil, errors.Wrap(err, "could not create new jwt")
+	}
+
+	relayURL := fmt.Sprintf("%s?%s", c.url, token)
+	log.Debug().Str("url", c.url).Msg("connecting")
+
+	con, resp, err := websocket.DefaultDialer.Dial(relayURL, nil)
+	if err != nil {
+		var body []byte
+		var status string
+		if resp != nil {
+			status = resp.Status
+			body, _ = io.ReadAll(resp.Body)
+		}
+
+		return nil, errors.Wrapf(err, "failed to connect (%s): %s", status, string(body))
+	}
+
+	if resp.StatusCode != http.StatusSwitchingProtocols {
+		return nil, fmt.Errorf("invalid response %s", resp.Status)
+	}
+
+	return con, nil
+}

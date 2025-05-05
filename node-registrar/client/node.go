103dceb7ac72dacb23cdc7d9d04fec2d88e35576
Eslam-Nawara <eslamnawara44@gmail.com>
2025-05-05 16:45:59 +0300

    handle old interface in registrar client

--- node-registrar/client/node.go
+++ node-registrar/client/node.go
@@ -4,9 +4,11 @@ import (
 	"bytes"
 	"encoding/json"
 	"fmt"
+	"io"
 	"net/http"
 	"net/url"
 	"reflect"
+	"strings"
 	"time"
 
 	"github.com/pkg/errors"
@@ -15,17 +17,8 @@ import (
 var ErrorNodeNotFound = fmt.Errorf("failed to get requested node from node registrar")
 
 // RegisterNode register physical/virtual nodes with on TFGrid.
-func (c *RegistrarClient) RegisterNode(
-	farmID uint64,
-	twinID uint64,
-	interfaces []Interface,
-	location Location,
-	resources Resources,
-	serialNumber string,
-	secureBoot,
-	virtualized bool,
-) (nodeID uint64, err error) {
-	return c.registerNode(farmID, twinID, interfaces, location, resources, serialNumber, secureBoot, virtualized)
+func (c *RegistrarClient) RegisterNode(node Node) (nodeID uint64, err error) {
+	return c.registerNode(node)
 }
 
 // UpdateNode update node configuration (farmID, interfaces, resources, location, secureBoot, virtualized).
@@ -186,79 +179,78 @@ func UpdateNodeWithHealthy() UpdateNodeOpts {
 	}
 }
 
-func (c *RegistrarClient) registerNode(
-	farmID uint64,
-	twinID uint64,
-	interfaces []Interface,
-	location Location,
-	resources Resources,
-	serialNumber string,
-	secureBoot,
-	virtualized bool,
-) (nodeID uint64, err error) {
+func (c *RegistrarClient) registerNode(node Node) (nodeID uint64, err error) {
 	err = c.ensureTwinID()
 	if err != nil {
 		return nodeID, errors.Wrap(err, "failed to ensure twin id")
 	}
+
 	url, err := url.JoinPath(c.baseURL, "nodes")
 	if err != nil {
 		return nodeID, errors.Wrap(err, "failed to construct registrar url")
 	}
 
-	data := Node{
-		FarmID:       farmID,
-		TwinID:       twinID,
-		Location:     location,
-		Resources:    resources,
-		Interfaces:   interfaces,
-		SecureBoot:   secureBoot,
-		Virtualized:  virtualized,
-		SerialNumber: serialNumber,
+	handler := func(body bytes.Buffer) (uint64, error) {
+		req, err := http.NewRequest("POST", url, &body)
+		if err != nil {
+			return nodeID, errors.Wrap(err, "failed to construct http request to the registrar")
+		}
+
+		authHeader, err := c.signRequest(time.Now().Unix())
+		if err != nil {
+			return nodeID, errors.Wrap(err, "failed to sign request")
+		}
+		req.Header.Set("X-Auth", authHeader)
+		req.Header.Set("Content-Type", "application/json")
+
+		resp, err := c.httpClient.Do(req)
+		if err != nil {
+			return nodeID, errors.Wrap(err, "failed to send request to registrer the node")
+		}
+
+		if resp == nil {
+			return 0, errors.New("no response received")
+		}
+		defer resp.Body.Close()
+
+		if resp.StatusCode != http.StatusCreated {
+			err = parseResponseError(resp.Body)
+			return 0, errors.Wrapf(err, "failed to create node on the registrar with status code %s", resp.Status)
+		}
+
+		result := struct {
+			NodeID uint64 `json:"node_id"`
+		}{}
+
+		err = json.NewDecoder(resp.Body).Decode(&result)
+		if err != nil {
+			return 0, errors.Wrap(err, "failed to decode response body")
+		}
+
+		return result.NodeID, nil
 	}
 
 	var body bytes.Buffer
-	err = json.NewEncoder(&body).Encode(data)
+	if err = json.NewEncoder(&body).Encode(node); err != nil {
+		return 0, err
+	}
+
+	nodeID, err = handler(body)
 	if err != nil {
-		return nodeID, errors.Wrap(err, "failed to encode request body")
+		// try node with old interface format
+		body, err := createRequestBodyWithOldInterfaceFormat(node)
+		if err != nil {
+			return nodeID, err
+		}
+
+		nodeID, err = handler(body)
+		if err != nil {
+			return nodeID, err
+		}
 	}
 
-	req, err := http.NewRequest("POST", url, &body)
-	if err != nil {
-		return nodeID, errors.Wrap(err, "failed to construct http request to the registrar")
-	}
-
-	authHeader, err := c.signRequest(time.Now().Unix())
-	if err != nil {
-		return nodeID, errors.Wrap(err, "failed to sign request")
-	}
-	req.Header.Set("X-Auth", authHeader)
-	req.Header.Set("Content-Type", "application/json")
-
-	resp, err := c.httpClient.Do(req)
-	if err != nil {
-		return nodeID, errors.Wrap(err, "failed to send request to registrer the node")
-	}
-
-	if resp == nil {
-		return 0, errors.New("no response received")
-	}
-	defer resp.Body.Close()
-
-	if resp.StatusCode != http.StatusCreated {
-		err = parseResponseError(resp.Body)
-		return 0, errors.Wrapf(err, "failed to create node on the registrar with status code %s", resp.Status)
-	}
-
-	result := struct {
-		NodeID uint64 `json:"node_id"`
-	}{}
-
-	err = json.NewDecoder(resp.Body).Decode(&result)
-	if err != nil {
-		return 0, errors.Wrap(err, "failed to decode response body")
-	}
-	c.nodeID = result.NodeID
-	return result.NodeID, err
+	c.nodeID = nodeID
+	return nodeID, nil
 }
 
 func (c *RegistrarClient) updateNode(opts []UpdateNodeOpts) (err error) {
@@ -279,37 +271,51 @@ func (c *RegistrarClient) updateNode(opts []UpdateNodeOpts) (err error) {
 
 	node = c.parseUpdateNodeOpts(node, opts)
 
+	handler := func(body bytes.Buffer) error {
+		req, err := http.NewRequest("PATCH", url, &body)
+		if err != nil {
+			return errors.Wrap(err, "failed to construct http request to the registrar")
+		}
+
+		authHeader, err := c.signRequest(time.Now().Unix())
+		if err != nil {
+			return errors.Wrap(err, "failed to sign request")
+		}
+		req.Header.Set("X-Auth", authHeader)
+		req.Header.Set("Content-Type", "application/json")
+
+		resp, err := c.httpClient.Do(req)
+		if err != nil {
+			return errors.Wrap(err, "failed to send request to update node")
+		}
+
+		if resp == nil {
+			return errors.New("no response received")
+		}
+		defer resp.Body.Close()
+
+		if resp.StatusCode != http.StatusOK {
+			err = parseResponseError(resp.Body)
+			return errors.Wrapf(err, "failed to update node with twin id %d with status code %s", c.twinID, resp.Status)
+		}
+		return nil
+	}
+
 	var body bytes.Buffer
-	err = json.NewEncoder(&body).Encode(node)
-	if err != nil {
-		return errors.Wrap(err, "failed to encode request body")
+	if err = json.NewEncoder(&body).Encode(node); err != nil {
+		return err
 	}
 
-	req, err := http.NewRequest("PATCH", url, &body)
-	if err != nil {
-		return errors.Wrap(err, "failed to construct http request to the registrar")
-	}
+	if err := handler(body); err != nil {
+		// try update with old interface format
+		node, err := createRequestBodyWithOldInterfaceFormat(node)
+		if err != nil {
+			return err
+		}
 
-	authHeader, err := c.signRequest(time.Now().Unix())
-	if err != nil {
-		return errors.Wrap(err, "failed to sign request")
-	}
-	req.Header.Set("X-Auth", authHeader)
-	req.Header.Set("Content-Type", "application/json")
-
-	resp, err := c.httpClient.Do(req)
-	if err != nil {
-		return errors.Wrap(err, "failed to send request to update node")
-	}
-
-	if resp == nil {
-		return errors.New("no response received")
-	}
-	defer resp.Body.Close()
-
-	if resp.StatusCode != http.StatusOK {
-		err = parseResponseError(resp.Body)
-		return errors.Wrapf(err, "failed to update node with twin id %d with status code %s", c.twinID, resp.Status)
+		if err := handler(node); err != nil {
+			return err
+		}
 	}
 
 	return
@@ -326,38 +332,58 @@ func (c *RegistrarClient) reportUptime(report UptimeReport) (err error) {
 		return errors.Wrap(err, "failed to construct registrar url")
 	}
 
-	var body bytes.Buffer
+	handler := func(body bytes.Buffer) error {
+		req, err := http.NewRequest("POST", url, &body)
+		if err != nil {
+			return errors.Wrap(err, "failed to construct http request to the registrar")
+		}
 
-	err = json.NewEncoder(&body).Encode(report)
-	if err != nil {
+		authHeader, err := c.signRequest(time.Now().Unix())
+		if err != nil {
+			return errors.Wrap(err, "failed to sign request")
+		}
+		req.Header.Set("X-Auth", authHeader)
+		req.Header.Set("Content-Type", "application/json")
+
+		resp, err := c.httpClient.Do(req)
+		if err != nil {
+			return errors.Wrap(err, "failed to send request to update uptime of the node")
+		}
+
+		if resp == nil {
+			return errors.New("no response received")
+		}
+		defer resp.Body.Close()
+
+		if resp.StatusCode != http.StatusCreated {
+			err = parseResponseError(resp.Body)
+			return errors.Wrapf(err, "failed to update node uptime for node with id %d with status code %s", c.nodeID, resp.Status)
+		}
+		return nil
+	}
+
+	var body bytes.Buffer
+	if err = json.NewEncoder(&body).Encode(report); err != nil {
 		return errors.Wrap(err, "failed to encode request body")
 	}
 
-	req, err := http.NewRequest("POST", url, &body)
-	if err != nil {
-		return errors.Wrap(err, "failed to construct http request to the registrar")
-	}
+	if err = handler(body); err != nil {
+		// try old report format time.Duration and time.Time
+		old := struct {
+			uptime    time.Duration
+			timestamp time.Time
+		}{
+			uptime:    time.Duration(report.Uptime),
+			timestamp: time.Now(),
+		}
+		var body bytes.Buffer
+		if err = json.NewEncoder(&body).Encode(old); err != nil {
+			return errors.Wrap(err, "failed to encode request body")
+		}
 
-	authHeader, err := c.signRequest(time.Now().Unix())
-	if err != nil {
-		return errors.Wrap(err, "failed to sign request")
-	}
-	req.Header.Set("X-Auth", authHeader)
-	req.Header.Set("Content-Type", "application/json")
-
-	resp, err := c.httpClient.Do(req)
-	if err != nil {
-		return errors.Wrap(err, "failed to send request to update uptime of the node")
-	}
-
-	if resp == nil {
-		return errors.New("no response received")
-	}
-	defer resp.Body.Close()
-
-	if resp.StatusCode != http.StatusCreated {
-		err = parseResponseError(resp.Body)
-		return errors.Wrapf(err, "failed to update node uptime for node with id %d with status code %s", c.nodeID, resp.Status)
+		if err = handler(body); err != nil {
+			return err
+		}
 	}
 
 	return
@@ -388,12 +414,17 @@ func (c *RegistrarClient) getNode(id uint64) (node Node, err error) {
 		return node, errors.Wrapf(err, "failed to get node with status code %s", resp.Status)
 	}
 
-	err = json.NewDecoder(resp.Body).Decode(&node)
+	bodyBytes, err := io.ReadAll(resp.Body)
 	if err != nil {
 		return
 	}
 
-	return
+	err = json.Unmarshal(bodyBytes, &node)
+	if err == nil {
+		return
+	}
+
+	return parseResponseBodyToNewInterfaceFormat(bodyBytes)
 }
 
 func (c *RegistrarClient) getNodeByTwinID(id uint64) (node Node, err error) {
@@ -448,9 +479,34 @@ func (c *RegistrarClient) listNodes(opts []ListNodeOpts) (nodes []Node, err erro
 		return nodes, errors.Wrapf(err, "failed to list nodes with with status code %s", resp.Status)
 	}
 
-	err = json.NewDecoder(resp.Body).Decode(&nodes)
+	bodyBytes, err := io.ReadAll(resp.Body)
 	if err != nil {
 		return
+	}
+
+	err = json.Unmarshal(bodyBytes, &nodes)
+	if err == nil {
+		return
+	}
+
+	var rawNodes []interface{}
+	err = json.Unmarshal(bodyBytes, &rawNodes)
+	if err != nil {
+		return
+	}
+
+	for _, node := range rawNodes {
+		nodeBytes, err := json.Marshal(node)
+		if err != nil {
+			return nodes, err
+		}
+
+		node, err := parseResponseBodyToNewInterfaceFormat(nodeBytes)
+		if err != nil {
+			return nodes, err
+		}
+
+		nodes = append(nodes, node)
 	}
 
 	return nodes, nil
@@ -561,3 +617,144 @@ func parseListNodeOpts(opts []ListNodeOpts) map[string]any {
 
 	return data
 }
+
+//
+// func migrateInterfaces(nodeMap map[string]any) (Node, error) {
+// 	type oldInterface struct {
+// 		Name string `json:"name"`
+// 		Mac  string `json:"mac"`
+// 		IPs  string `json:"ips"`
+// 	}
+//
+// 	rawInterfaces, ok := nodeMap["interfaces"].([]interface{})
+// 	if !ok {
+// 		return Node{}, fmt.Errorf("interfaces is not a list")
+// 	}
+//
+// 	interfaceBytes, err := json.Marshal(rawInterfaces)
+// 	if err != nil {
+// 		return Node{}, fmt.Errorf("failed to marshal interfaces: %w", err)
+// 	}
+//
+// 	var old []oldInterface
+// 	if err := json.Unmarshal(interfaceBytes, &old); err != nil {
+// 		return Node{}, errors.New("node interfaces doesn't implement either old or new Interface struct")
+// 	}
+// 	var newInterfaces []Interface
+// 	for _, ifs := range old {
+// 		ips := strings.Split(ifs.IPs, "/")
+// 		newI := Interface{
+// 			Name: ifs.Name,
+// 			Mac:  ifs.Mac,
+// 			IPs:  ips,
+// 		}
+// 		newInterfaces = append(newInterfaces, newI)
+// 	}
+// 	nodeMap["interfaces"] = newInterfaces
+//
+// 	encodedNode, err := json.Marshal(nodeMap)
+// 	if err != nil {
+// 		return Node{}, err
+// 	}
+//
+// 	var updatedNode Node
+// 	err = json.Unmarshal(encodedNode, updatedNode)
+//
+// 	return updatedNode, err
+// }
+
+/*
+*
+*
+*
+*
+*
+*
+*
+*
+ */
+
+type oldInterfaceFormat struct {
+	Name string `json:"name"`
+	Mac  string `json:"mac"`
+	IPs  string `json:"ips"`
+}
+
+func createRequestBodyWithOldInterfaceFormat(node Node) (body bytes.Buffer, err error) {
+	var oldInterfaces []oldInterfaceFormat
+	interfaces := node.Interfaces
+	for _, i := range interfaces {
+		old := oldInterfaceFormat{
+			Name: i.Name,
+			Mac:  i.Mac,
+			IPs:  strings.Join(i.IPs, "/"),
+		}
+		oldInterfaces = append(oldInterfaces, old)
+	}
+
+	nodeBytes, err := json.Marshal(node)
+	if err != nil {
+		return
+	}
+
+	var nodeMap map[string]any
+	err = json.Unmarshal(nodeBytes, &nodeMap)
+	if err != nil {
+		return
+	}
+	nodeMap["interfaces"] = oldInterfaces
+
+	err = json.NewEncoder(&body).Encode(node)
+	if err != nil {
+		return
+	}
+
+	return
+}
+
+func parseResponseBodyToNewInterfaceFormat(nodeBytes []byte) (Node, error) {
+	oldFormatNode := struct {
+		Interfaces []oldInterfaceFormat `json:"interfaces"`
+	}{}
+
+	err := json.Unmarshal(nodeBytes, &oldFormatNode)
+	if err != nil {
+		return Node{}, err
+	}
+
+	var newFormat []Interface
+	for _, i := range oldFormatNode.Interfaces {
+		ips := strings.Split(i.IPs, "/")
+		newFormat = append(newFormat, Interface{
+			Name: i.Name,
+			Mac:  i.Mac,
+			IPs:  ips,
+		})
+	}
+
+	var nodeMap map[string]any
+	err = json.Unmarshal(nodeBytes, &nodeMap)
+	if err != nil {
+		return Node{}, err
+	}
+	nodeMap["interfaces"] = newFormat
+
+	encodedNode, err := json.Marshal(nodeMap)
+	if err != nil {
+		return Node{}, err
+	}
+
+	var node Node
+	err = json.Unmarshal(encodedNode, &node)
+	if err != nil {
+		return Node{}, err
+	}
+
+	return node, nil
+}
+
+// nodeBytes, err := json.Marshal(nodeMap)
+// if err != nil {
+// 	return Node{}, err
+// }
+//
